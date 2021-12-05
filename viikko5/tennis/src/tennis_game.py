f1432301c115eda9be04cf80e45e920505326873
Natalie Johansson <mnrjohansson@gmail.com>
2021-12-05 13:21:15 +0200

    tennis

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -2,56 +2,57 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_points = 0
+        self.player2_points = 0
+        self.score_names = {
+            0: "Love",
+            1: "Fifteen",
+            2: "Thirty",
+            3: "Forty",
+            4: "Deuce"
+        }
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+            self.player1_points += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player2_points += 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+        if self.players_are_tied():
+            return self.get_tied_score(self.player1_points)
+        elif self.a_player_scored_more_than_four_points():
+            return self.get_advantage_score(self.player1_points, self.player2_points)
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+            return self.get_regular_score(self.player1_points, self.player2_points)
+    
+    def players_are_tied(self):
+        return self.player1_points == self.player2_points
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+    def get_tied_score(self, points):
+        result = self.score_names[points] + "-All"
+        if points > 3:
+            result = "Deuce"
+        return result
 
-        return score
+    def a_player_scored_more_than_four_points(self):
+        return self.player1_points >= 4 or self.player2_points >= 4
+
+    def get_advantage_score(self, points1, points2):
+        point_difference = abs(points1 - points2)
+        result = ""
+        
+        if point_difference == 1:
+            result += "Advantage "
+        else:
+            result += "Win for "
+        
+        if points1 > points2:
+            result += "player1"
+        else:
+            result += "player2"
+        
+        return result
+    
+    def get_regular_score(self, points1, points2):
+        return self.score_names[points1] + "-" + self.score_names[points2]
